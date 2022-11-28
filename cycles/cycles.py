87a39aae0737256eed7fa5e2af281626ab2c0b4e
edwarda21 <edwarda21@sps-prosek.cz>
2022-11-28 10:36:39 +0100

    modified cycles.py

--- cycles/cycles.py
+++ cycles/cycles.py
@@ -1,47 +1,47 @@
 def square(side):  # task 1
     for i in range(side):
-        print("#"*side)
+        print("#" * side)
 
 
 def empty_square(side):  # task 2
-    print("#"*side)
-    for i in range(side-2):
-        print("#"+" "*(side-2)+"#")
+    print("#" * side)
+    for i in range(side - 2):
+        print("#" + " " * (side - 2) + "#")
     print("#" * side)
 
 
 def right_angle_pyramid(height):  # task 3
     for i in range(height):
-        print("#"*(i+1))
+        print("#" * (i + 1))
 
 
 def pyramid(height):  # task 4
-    final_width = 2*height-1
+    final_width = 2 * height - 1
     for i in range(height):
-        i+=1
-        number_of_hashes = 2*i-1
-        number_of_spaces = (final_width-number_of_hashes)//2
-        print("."*number_of_spaces+"#"*number_of_hashes+"."*number_of_spaces)
+        i += 1
+        number_of_hashes = 2 * i - 1
+        number_of_spaces = (final_width - number_of_hashes) // 2
+        print("." * number_of_spaces + "#" * number_of_hashes + "." * number_of_spaces)
 
 
 def cross(length):  # task 5
     i = 0
-    x = length-2
-    if length%2==0: 
+    x = length - 2
+    if length % 2 == 0:
         modifier = -1
-    else: 
+    else:
         modifier = 0
-    while i < length//2+modifier:
-        print(" "*i+"#"+" "*x+"#")
-        i+=1
-        x -=2
-    print(" "*i+"#"+" "*x)
-    i-=1
-    x+=2
-    while -1<i < length:
-        print(" "*i+"#"+" "*x+"#")
-        i-=1
-        x +=2
+    while i < length // 2 + modifier:
+        print(" " * i + "#" + " " * x + "#")
+        i += 1
+        x -= 2
+    print(" " * i + "#" + " " * x)
+    i -= 1
+    x += 2
+    while -1 < i < length:
+        print(" " * i + "#" + " " * x + "#")
+        i -= 1
+        x += 2
 
 
 def diamond(side):  # task 6
@@ -51,20 +51,20 @@ def diamond(side):  # task 6
         number_of_hashes = 2 * i - 1
         number_of_spaces = (final_width - number_of_hashes) // 2
         print("." * number_of_spaces + "#" * number_of_hashes + "." * number_of_spaces)
-    for x in range(side-1,0,-1):
+    for x in range(side - 1, 0, -1):
         number_of_hashes = 2 * x - 1
         number_of_spaces = (final_width - number_of_hashes) // 2
-        print("."*number_of_spaces+"#"*number_of_hashes+ "." * number_of_spaces)
+        print("." * number_of_spaces + "#" * number_of_hashes + "." * number_of_spaces)
 
 
 def circle(radius):  # task 7
     for i in range(radius):
-        print("#"*radius)
-    #nedostatek mozkove kapacity ve 22:20 :D
+        print("#" * radius)
+    # nedostatek mozkove kapacity ve 22:20 :D
 
 
 def chess_board_fixed(side):  # task 8
-    char_dict = {1:"#",-1:"."}
+    char_dict = {1: "#", -1: "."}
     current_char = 1
     for i in range(side):  # loop through number of rows
         row = ""
@@ -72,38 +72,38 @@ def chess_board_fixed(side):  # task 8
             row += char_dict[current_char]
             current_char *= -1
         print(row)
-        if(side%2==0): current_char *= -1
-        
-        
+        if (side % 2 == 0): current_char *= -1
+
+
 def chess_board_resizable(side, space_size):  # task 9
-    char_dict = {1:"#",-1:"."}
+    char_dict = {1: "#", -1: "."}
     current_char = 1
     for i in range(side):  # loop through number of rows
         row = ""
         for x in range(side):  # create row
-            row += char_dict[current_char]*space_size
+            row += char_dict[current_char] * space_size
             current_char *= -1
-        print("\n".join([row]*space_size))
-        if(side%2==0): current_char *= -1
-        
-        
-n = 11       
-m = 5        
+        print("\n".join([row] * space_size))
+        if (side % 2 == 0): current_char *= -1
+
+
+n = 11
+m = 5
 
 square(n)
-print("-"*20)
+print("-" * 20)
 empty_square(n)
-print("-"*20)
+print("-" * 20)
 right_angle_pyramid(n)
-print("-"*20)
+print("-" * 20)
 pyramid(n)
-print("-"*20)
+print("-" * 20)
 cross(n)
-print("-"*20)
+print("-" * 20)
 diamond(n)
-print("-"*20)
+print("-" * 20)
 circle(n)
-print("-"*20)
+print("-" * 20)
 chess_board_fixed(n)
-print("-"*20)
-chess_board_resizable(n,m)
+print("-" * 20)
+chess_board_resizable(n, m)
