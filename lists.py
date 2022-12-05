f6afa4ba937a600ba3fa6c08f7da97bb56283ad4
edwarda21 <edwarda21@sps-prosek.cz>
2022-12-05 11:45:38 +0100

    commit

--- lists.py
+++ lists.py
@@ -1,4 +1,16 @@
-list = []
-print(list)
-list.append("something")
-print(list)
+def partition(list_to_sort):
+    left_pointer = 0
+    right_pointer = len(list_to_sort)-2
+    print(list_to_sort[right_pointer])
+    buffer_val = list_to_sort[right_pointer+1]
+    while right_pointer > left_pointer:
+        print(left_pointer)
+        while list_to_sort[left_pointer] < list_to_sort[right_pointer]:
+            left_pointer += 1
+            print(left_pointer)
+        while list_to_sort[right_pointer] > list_to_sort[left_pointer]:
+            right_pointer -= 1
+            print(right_pointer)
+
+
+partition([7489,5,4684,7646,12,31,2,35,8,4,8,9,6,64,3])
