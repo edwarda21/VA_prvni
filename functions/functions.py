1f8fce912f28e7bed42a60815c78846e9eb3f46a
edwarda21 <edwarda21@sps-prosek.cz>
2022-12-12 11:39:37 +0100

    created a badass fibonacci sequence function with memoization and started hangman console game

---
+++ functions/functions.py
@@ -0,0 +1,72 @@
+import random
+
+# define a function using the def keyword
+# example: function to calculate the area of a square or rectangle
+def calculate_area(a, b=None):
+    if b is None:
+        return a * a
+    return b * a
+
+
+print(calculate_area(a=8))
+
+fib_memo = {}
+
+
+def fib(n, memo):
+    if 1 == n or n == 0:
+        return n
+    elif n in memo:
+        return memo[n]
+    else:
+
+        memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+        return memo[n]
+
+
+for i in range(1000000):
+    print(fib(i, fib_memo))
+
+
+# create a function that accepts and infinite amount of number to add up with an addition keyword argument
+def add(*number, multiplication=1):
+    output = 0
+    for i in number:
+        output += i
+    return output * multiplication
+
+
+# this allows to add as many numbers as we want, and we can define multiplication by using the keyword
+# print(add(1, 2, 3, 4, 5, multiplication=5))
+
+# create rock,paper, scissors (need random library that is imported at the start of file)
+
+
+def rps():
+    user_move = input('''Please select one of the following as your move:
+    (R)ock
+    (P)aper
+    (S)cissors =>''')
+    user_move = user_move.lower()[0]
+    moves = ["r","p","s"]
+    print (user_move)
+    computer_move = random.choice(moves)
+    if user_move ==  computer_move:
+        return f"Draw, the computer also chose {computer_move}"
+    elif user_move == "r":
+        if (computer_move == "p"):
+            return "Sorry you lose the computer chose paper"
+        else:
+            return "Congratulations you won!"
+    elif user_move == "p":
+        if computer_move == "s":
+            return "Sorry, you lose. The computer chose scissors"
+        else:
+            return "Congratulations! You won"
+    elif user_move == "s":
+        if computer_move == "r":
+            return "Sorry, you lose. The computer chose rock"
+        else:
+            return "Congratulation! You won"
+
+# print(rps())
---
+++ functions/hangman.py
@@ -0,0 +1,55 @@
+import random
+'''
+Create hangman game
+Number of tries = 7
+Steps:
+    1. Get random word * 
+    2. Convert to letters * 
+    3. Check input is a letter from the alphabet *
+    4. Check letter is in word * 
+    5. Save guessed letters 
+    6. Save wrongly guessed letters * 
+'''
+# define variables and lists
+guessed_letters = []
+wrongly_guessed_letters = []
+health = 7
+play = True # creates the game loop
+words = [
+    "cat",
+    "hungover",
+    "apple",
+    "horse",
+    "configure",
+    "automate",
+    "capitalize"
+]
+word = list(random.choice(words))
+# get length of word
+word_len = len(word)
+letters_to_guess = set(word)
+# give user info
+user_guess = ["_" for i in range(word_len)]
+print (f'''
+The word you are guessing is {word_len} letters long.
+{''.join(user_guess)}
+''')
+while play:
+    letter = input(f"What letter are you going to guess? => ").lower()
+    if len(letter) == 0 or not letter[0].isalpha():
+        continue
+    else:
+        letter = letter[0]
+        if letter not in letters_to_guess:
+            health -= 1
+            if letter in wrongly_guessed_letters:
+                print(f'''You already guessed {letter} and it is still wrong!
+                You have {health} tries remaining''')
+            else:
+                wrongly_guessed_letters.append(letter)
+                print(f'''Sorry, {letter} is not in the word.
+                You have {health} tries remaining''')
+        else:
+            for letter in
+
+
