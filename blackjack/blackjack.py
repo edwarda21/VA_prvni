36517b4e3e5cb713db6241a314221c7922b0c367
edwarda21 <edwarda21@sps-prosek.cz>
2023-01-09 11:42:26 +0100

    created a deck for blackjack.py

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -3,4 +3,5 @@
 values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 colours = ["H", "C", "S", "D"]
 deck = [[{"value": value, "colour": colour} for value in values] for colour in colours]
+
 print(deck)
