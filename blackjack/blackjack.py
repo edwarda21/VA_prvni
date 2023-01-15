0dc131f387127f7d160c964cbf9527b07597974a
edwarda21 <edwarda21@sps-prosek.cz>
2023-01-15 22:24:23 +0100

    aces don't be wildin anymore. Have to add evaluation step to code

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -3,17 +3,18 @@ import random
 
 
 def get_decks(in_number_of_decks):
-    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "A", "A", "A"]
     colours = ["H", "C", "S", "D"]
     out_deck = []
     for decks in range(0, in_number_of_decks):
-        out_deck += [[{"value": value, "colour": colour} for value in values] for colour in colours]
+        out_deck += [[{"value": value, "colour": colour, "type": "soft"} for value in values] for colour in colours]
     return out_deck
 
 
 def find_ace(in_hand):
     for card in in_hand:
-        if card["value"] == "A":
+        if card["value"] == "A" and card["type"] == "soft":
+            card["type"] = "hard"
             return True
     return False
 
@@ -43,20 +44,28 @@ class Player:
         except ValueError:
             if card["value"] in ["J", "Q", "K"]:
                 self.val_of_hand += 10
+            elif self.val_of_hand + 11 > 21:
+                self.val_of_hand += 1
+                card["type"] = "hard"
             else:
                 self.val_of_hand += 11
         finally:
             self.hand.append(card)
             if self.val_of_hand > 21:
-                if not find_ace(self.hand):
-                    return False
-                else:
+                if find_ace(self.hand):
                     self.val_of_hand -= 10
+                    return True
+                else:
+                    return False
             else:
                 return True
 
         # gets the value of drawn card, if the value is not a number then value is assigned manually, hence try except
         # finally clause there to always check the value of the persons hand see if they are bust or not
+
+    def check_win(self):
+        if self.val_of_hand == 21 and len(self.hand) == 2:
+            return True
 
 
 no_of_decks = 6
@@ -69,31 +78,37 @@ dealer = Player()
 # create a game loop to play around with
 dealer.draw_card(deck)
 player1.get_hand(deck)
-print(f"The dealers card is {dealer.hand}")
+pprint(f"The dealers card is {dealer.hand}")
 while True:
     print(f"The value of your hand is {player1.val_of_hand}")
-    print(f"Your cards are {player1.hand}")
+    print("Your cards are:")
+    pprint(player1.hand)
+    if player1.check_win():
+        print("BLACKJACK! You now have a great chance of winning")
+        break
     while True:
-        h_or_s = input("Do you want to (H)it or (S)tand?\n")
-        if h_or_s.lower()[0] in ["h", "s"]:
+        h_or_s = input("Do you want to (H)it or (S)tand?\n =>")
+        if len(h_or_s) > 0 and h_or_s.lower()[0] in ["h", "s"]:
             break
     if h_or_s == "s":
         break
     if not player1.draw_card(deck):
-        print(f"You have gone bust, the card you last drew was a {player1.hand[len(player1.hand)-1]}")
+        print(f"You have gone bust, the card you last drew was a {player1.hand[len(player1.hand) - 1]['value']}")
+        print(f"Your final hand value was {player1.val_of_hand}")
         break
 
 dealer.draw_card(deck)
 while True:
     # dealers game
+    if dealer.check_win():
+        print("The dealer got a blackjack!")
+        break
     if dealer.val_of_hand <= 16:
-        print(dealer.val_of_hand)
-        print("Card is drawn")
         if not dealer.draw_card(deck):
             print(f"The dealer has gone bust their final hand is {dealer.hand}")
             break
-        print(dealer.val_of_hand)
     else:
         print(f"The dealers final hand is {dealer.hand}")
         print(f"The dealers final hand value is {dealer.val_of_hand}")
         break
+#
