5eb74b42a6f7c985cfa1a299ac879e5d29579c92
edwarda21 <edwarda21@sps-prosek.cz>
2023-01-16 11:46:47 +0100

    change how to get card because all you did was useless in the end

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -3,11 +3,12 @@ import random
 
 
 def get_decks(in_number_of_decks):
-    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "A", "A", "A"]
+    values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     colours = ["H", "C", "S", "D"]
     out_deck = []
     for decks in range(0, in_number_of_decks):
-        out_deck += [[{"value": value, "colour": colour, "type": "soft"} for value in values] for colour in colours]
+        out_deck += [{"value": value, "colour": colour, "type": "soft"} for value in values for colour in colours]
+    random.shuffle(out_deck)
     return out_deck
 
 
@@ -19,10 +20,17 @@ def find_ace(in_hand):
     return False
 
 
+def write_hand(in_hand):
+    for card in in_hand:
+        continue
+
+
 class Player:
     def __init__(self):
         self.hand = []
-        self.val_of_hand = 0
+        self.count = 0
+        self.blackjack = False
+        self.bust = False
 
     def get_hand(self, in_deck):
         for x in range(0, 2):
@@ -30,6 +38,7 @@ class Player:
 
     def draw_card(self, in_deck):
         card = random.choice(random.choice(in_deck))
+        # TODO use pop to get random card you faggot
         for suit in deck:
             try:
                 suit.remove(card)
@@ -40,36 +49,38 @@ class Player:
         # gets a random card from deck and then removes it.
         # deck is list created of suits (lists) try and except block to check if card can be removed from suit
         try:
-            self.val_of_hand += int(card["value"])
+            self.count += int(card["value"])
         except ValueError:
             if card["value"] in ["J", "Q", "K"]:
-                self.val_of_hand += 10
-            elif self.val_of_hand + 11 > 21:
-                self.val_of_hand += 1
+                self.count += 10
+            elif self.count + 11 > 21:
+                self.count += 1
                 card["type"] = "hard"
             else:
-                self.val_of_hand += 11
+                self.count += 11
         finally:
             self.hand.append(card)
-            if self.val_of_hand > 21:
+            if self.count > 21:
                 if find_ace(self.hand):
-                    self.val_of_hand -= 10
+                    self.count -= 10
                     return True
                 else:
+                    self.bust = True
                     return False
             else:
                 return True
 
         # gets the value of drawn card, if the value is not a number then value is assigned manually, hence try except
         # finally clause there to always check the value of the persons hand see if they are bust or not
-
-    def check_win(self):
-        if self.val_of_hand == 21 and len(self.hand) == 2:
+    def check_blackjack(self):
+        if self.count == 21 and len(self.hand) == 2:
+            self.blackjack = True
             return True
 
 
 no_of_decks = 6
 deck = get_decks(no_of_decks)
+pprint(deck)
 player1 = Player()
 dealer = Player()
 
@@ -80,10 +91,10 @@ dealer.draw_card(deck)
 player1.get_hand(deck)
 pprint(f"The dealers card is {dealer.hand}")
 while True:
-    print(f"The value of your hand is {player1.val_of_hand}")
+    print(f"The value of your hand is {player1.count}")
     print("Your cards are:")
     pprint(player1.hand)
-    if player1.check_win():
+    if player1.check_blackjack():
         print("BLACKJACK! You now have a great chance of winning")
         break
     while True:
@@ -94,21 +105,35 @@ while True:
         break
     if not player1.draw_card(deck):
         print(f"You have gone bust, the card you last drew was a {player1.hand[len(player1.hand) - 1]['value']}")
-        print(f"Your final hand value was {player1.val_of_hand}")
         break
 
 dealer.draw_card(deck)
 while True:
     # dealers game
-    if dealer.check_win():
+    if dealer.check_blackjack():
         print("The dealer got a blackjack!")
         break
-    if dealer.val_of_hand <= 16:
+    if dealer.count <= 16:
         if not dealer.draw_card(deck):
             print(f"The dealer has gone bust their final hand is {dealer.hand}")
             break
     else:
         print(f"The dealers final hand is {dealer.hand}")
-        print(f"The dealers final hand value is {dealer.val_of_hand}")
+        print(f"The dealers final hand value is {dealer.count}")
         break
-#
+# evaluate code and ask to play again
+if player1.bust:
+    print("You have lost the game.")
+elif dealer.bust:
+    print("Congratulations! You win.")
+elif dealer.count == 21 == player1.count:
+    if player1.blackjack and not dealer.blackjack:
+        print("Congratulations! You win the game with a blackjack.")
+    elif dealer.blackjack and not player1.blackjack:
+        print("You have lost the game.")
+    else:
+        print("Unlucky, the game ended in a draw.")
+elif player1.count > dealer.count:
+    print("Congratulations! You win.")
+else:
+    print("You have lost the game.")
