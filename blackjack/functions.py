47f1b82fb41eb8d5354c70c159fcb56f2ad2224f
edwarda21 <edwarda21@sps-prosek.cz>
2023-01-30 10:37:55 +0100

    added a save info function to the player object, returning player function has been fixed, and some other modifications for ease of use and change in the future

--- blackjack/functions.py
+++ blackjack/functions.py
@@ -2,6 +2,10 @@ import random
 import re
 import json
 
+PLAYERS_FILE = "players.json"
+STARTING_TOKENS = 20
+BASE_TOKENS = 12
+
 
 def get_decks(in_number_of_decks):
     values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
@@ -27,7 +31,7 @@ def write_hand(in_hand):
 
 
 class Player:
-    def __init__(self, nick):
+    def __init__(self, nick, tokens=STARTING_TOKENS, won=0, lost=0, max_tokens=STARTING_TOKENS):
         self.hand = []
         self.count = 0
         self.blackjack = False
@@ -35,7 +39,10 @@ class Player:
         self.nick = nick
         self._bet = None
         self.bet_multiplier = 1
-        self.tokens = 0
+        self.tokens = tokens
+        self.won = won
+        self.lost = lost
+        self.max_tokens = max_tokens
 
     def get_hand(self, in_deck):
         for x in range(0, 2):
@@ -81,64 +88,94 @@ class Player:
             self.bet *= 2
             return False if self.bust else True
 
+    def save_info(self, list_of_players):
+        current_player = list_of_players[self.nick]
+        current_player["tokens"] = self.tokens
+        current_player["won"] = self.won
+        current_player["lost"] = self.lost
+        if self.max_tokens > current_player["max"]:
+            current_player["max"] = self.max_tokens
+        write_to_json(PLAYERS_FILE, list_of_players)
+
     @property
     def bet(self):
         return self._bet
 
     @bet.setter
     def bet(self, bet):
-        if bet < 5 or not isinstance(bet, int):
-            raise ValueError("You may not bet less than 5$")
+        if bet < 1 or not isinstance(bet, int):
+            raise ValueError("You may not bet less than 1$")
         else:
             self._bet = bet
+
+    def write_statistic(self):
+        output = f'''
+                    |''', "-" * 30, f'''
+                    | Nickname: {self.name}
+                    | Amount of tokens: {self.tokens}
+                    | Games won: {self.won}
+                    | Games lost: {self.lost}
+                    | All time most tokens: {self.max_tokens}
+                    
+        
+        '''
 
 
 def create_player(player_list):
     while True:
         in_name = input(
-            '''Well hello player, what will your nickname be? (at least 3 characters long with at least 1 character) \n =>''')
+            '''Well hello player, what will your nickname be? (at least 3 characters long with at least 1 character) 
+            \n =>''')
         if len(in_name) >= 3 and not in_name.isspace() and " " not in in_name and re.search('[a-zA-Z]', in_name):
-            if check_player_exists(in_name,player_list):
+            if check_player_exists(in_name, player_list):
                 print("Sorry, this username is already taken, try choosing something else.")
             else:
-                print(f"That is a great username, welcome {in_name}.")
+                print(f"That is a great username!\n")
                 break
         else:
             print("Sorry, your username has to be at least 3 characters long and must not include whitespace")
-
-    player = Player(in_name)
-    add_player(player,player_list)
-    return Player
-
+    # runs a loop until the player puts in a valid username that can be saved
+    out_player = Player(in_name)
+    print(
+        f"Welcome {out_player}! Glad to have you onboard. We have given you {STARTING_TOKENS} tokens as a welcome gift.")
+    player_list[out_player.nick] = {"tokens": STARTING_TOKENS, "won": 0, "lost": 0, "max": STARTING_TOKENS}
+    write_to_json(PLAYERS_FILE, player_list)
+    return out_player
+    # creates a new player instance and automatically saves it to the players file
 
 
 def get_players(file):
     with open(file) as f:
         content = f.read()
-
         return json.loads(content) if len(content) > 0 else {}
+    # return the list of players from the players.json file
 
 
 def write_to_json(file, content):
     content = json.dumps(content)
     with open(file, "w+") as f:
         return f.write(content)
+    # rewrites the players.json file with new or updated content
 
 
-def add_player(in_player, player_list):
-    if in_player.nick in player_list:
-        print("Sorry, cant add your player, this nick is already taken")
-        return False
-    else:
-        player_list[in_player.nick] = {"tokens": 5,"won":0,"lost":0}
-        return True
-    # creates a new player to the list
-
-def check_player_exists(name,player_list):
+def check_player_exists(name, player_list):
     if name in player_list:
         return True
     return False
     # checks if a player exists by seeing if there is a key in the players.json
 
 
+def returning_player(player_list):
+    while True:
+        in_name = input(
+            '''Please tell us what your nick is, player. 
+            \n =>''')
 
+        if in_name in player_list:
+            print(f"Welcome back {in_name}!")
+            return Player(in_name, player_list[in_name]["tokens"], player_list[in_name]["won"],
+                          player_list[in_name]["lost"],
+                          player_list[in_name]["max"])
+        else:
+            print("Sorry, there is no player with this nick")
+    # checks if the inputted name is from a returning player or not, if not then asks for the nick again
