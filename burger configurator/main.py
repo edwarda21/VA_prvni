442d83b91d290f52ac23cd3f6974b3f222c0f7e8
edwarda21 <edwarda21@sps-prosek.cz>
2023-02-20 11:40:19 +0100

    added combobox and learned some new stuff

--- burger configurator/main.py
+++ burger configurator/main.py
@@ -1,9 +1,21 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 
 window = tk.Tk()
 window.title("Hello")
+window.winfo_height()
 # TODO: create form GUI
-form = tk.Frame()
+meat_options = ("chicken","beef","veal","pork")
+form = tk.Frame(bg="red")
+name_lbl = ttk.Label(master=form, text="Burger name:")
+name_ent = ttk.Entry(master=form)
+meat_lbl = ttk.Label(master=form, text="Meat")
+meal_cmb = ttk.Combobox(master=form, values = meat_options)
+name_lbl.pack()
+name_ent.pack()
+meat_lbl.pack()
+meal_cmb.pack()
+form.pack(fill=tk.BOTH, expand=True)
 # TODO: create result GUI
 # window mainloop()
 window.mainloop()
