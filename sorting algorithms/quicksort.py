8e971d5ef2a8c23df3a5052dcd4d145473ffa77c
edwarda21 <edwarda21@sps-prosek.cz>
2023-02-27 11:47:09 +0100

    it not working

--- sorting algorithms/quicksort.py
+++ sorting algorithms/quicksort.py
@@ -1,23 +1,33 @@
 def partition(to_sort):
+    print("Partition no. ", partition.counter)
+    partition.run = 1
+    partition.counter += 1
     pivot = to_sort[0]
-    left = 0
+    left = 1
     right = len(to_sort) - 1
+    print("Pivot", pivot)
     while left < right:
+        print(f"Partition run No. {partition.run}")
+        print("List:", to_sort)
+        partition.run +=1
         while to_sort[left] < pivot:
             left += 1
         while to_sort[right] > pivot:
             right -= 1
-        to_sort[left], to_sort[right] = to_sort[right], to_sort[left]
-    print(to_sort[:left])
-    print(to_sort[right+1:])
-    return to_sort[:left],to_sort[right+1:],left
+        if left < right:
+            to_sort[left], to_sort[right] = to_sort[right], to_sort[left]
+    to_sort[0],to_sort[right] = to_sort[right],pivot
+    print("End of partition:")
+    print(to_sort)
+    return to_sort[:right], to_sort[left+1:], [to_sort[right]]
 
+
+partition.counter = 1
+partition.run = 1
 
 
 def quicksort(to_sort):
     print("QS")
-    print(to_sort)
-    print(len(to_sort))
     if len(to_sort) == 1:
         return to_sort
     else:
@@ -25,4 +35,4 @@ def quicksort(to_sort):
         return quicksort(left) + pivot + quicksort(right)
 
 
-print(quicksort([8,849,48,1,8,97,0,54,48,23,12]))
+print(quicksort([8, 849, 48, 1, 7, 97, 0, 54, 48, 23, 12]))
