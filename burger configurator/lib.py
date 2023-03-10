7598678758fe8feaa61f55607b2688bfc3b066a4
edwarda21 <edwarda21@sps-prosek.cz>
2023-03-10 23:20:27 +0100

    changed add_elements function, continue on with create_burger

--- burger configurator/lib.py
+++ burger configurator/lib.py
@@ -1,14 +1,22 @@
 import tkinter.ttk as ttk
+import tkinter as tk
 
 
-def create_burger():
-    print("Hello")
+def create_burger(elements):
+    for category in elements:
+        for element in category:
+            print(element.get())
 
 
-def add_elements(frame,values, text="", list_of_elements=[]):
-    lbl = ttk.Label(master=frame, text=text+f":")
+def add_elements(frame, values, text="", list_of_elements=[]):
+
+    lbl = ttk.Label(master=frame, text=text + f":")
     cmb = ttk.Combobox(master=frame, values=values)
     if text != "":
-        lbl.pack()
-    cmb.pack()
+        lbl.grid(row=frame.grid_size()[1]+1, column=0)
+    col = 1
+    if len(frame.grid_slaves(frame.grid_size()[1])) == 1:
+        col = 2
+    cmb.grid(row=frame.grid_size()[1]+1, column= col)
     list_of_elements.append(cmb)
+    print(cmb.grid_info())
