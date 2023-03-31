943f74a409db4ce12c270255fd33ab4c2a463f52
edwarda21 <edwarda21@sps-prosek.cz>
2023-04-01 01:52:38 +0200

    I am trying and am getting somewhere, need to work out adding to the bloody stored equation

--- calculator/calc.py
+++ calculator/calc.py
@@ -1,6 +1,7 @@
 from tkinter import Tk
 from functions import *
 win = Tk()
+win.geometry("500x500")
 calc = Calculator(win)
 calc.build()
 
--- calculator/functions.py
+++ calculator/functions.py
@@ -1,4 +1,19 @@
 from tkinter import *
+from tkinter.ttk import *
+
+
+def string_difference(string1, string2):
+    length = min(len(string1), len(string2))
+
+    difference = ""
+    for i in range(length):
+        if string1[i] != string2[i]:
+            difference += string2[i]
+    if len(string1) > len(string2):
+        difference += string1[length:]
+    else:
+        difference += string2[length:]
+    return difference
 
 
 class Calculator:
@@ -7,29 +22,66 @@ class Calculator:
         self.memory = None
         self.window = window
         self.layout = [
-            ["AC", "Ans", "**2", "/"],
+            ["AC", "Ans", "x²", "/"],
             ["7", "8", "9", "+"],
             ["4", "5", "6", "-"],
             ["1", "2", "3", "*"],
-            ["0", ".", "=", "√"]
+            ["0", ".", "=", "√x"]
+        ]
+        self.operations = {
+            "+": "+",
+            "-": "-",
+            "*": "*",
+            "x²": "(x**2)",
+            "√x": "(x**(1/2))",
+            "/": "/",
+
+        }
+        self.actions = [
+            "AC",
+            "Ans",
+            "="
         ]
         self.main = Frame()
+        self.entry_b = None
+        self.equation = []
 
     def build(self):
         # entry box for equation
-        entry = Entry(self.window)
-        entry.pack()
+        self.entry_b = Entry(self.window)
+        self.entry_b.pack(fill=BOTH)
         # frame for buttons
         btn_frame = Frame(self.window)
-        btn_frame.pack()
+
         # generate buttons based on layout
         for i, btn_row in enumerate(self.layout):
             row = i + 1
             for i, l_btn in enumerate(btn_row):
                 column = i + 1
-                print(l_btn)
                 btn = Button(btn_frame, text=l_btn, command=lambda val=l_btn: self.btn_press(val))
-                btn.grid(row=row, column=column)
+                btn.grid(row=row, column=column, ipadx=10, ipady=5)
+        btn_frame.pack(pady=15)
 
     def btn_press(self, btn):
-        print(btn)
+        if btn.isdigit():
+            self.entry_b.insert(END, btn)
+        else:
+            self.get_last_expression(btn)
+
+
+    def get_last_expression(self, value):
+        ent_box_eq = self.entry_b.get()
+        # get difference between stored equation and entry box equation
+        diff = string_difference("".join(self.equation), ent_box_eq)
+        print(f"STORED EQUATION: {self.equation}")
+        print(f"ENTRY BOX EQUATION: {self.entry_b.get()}")
+        print(f"DIFFERENCE:{diff}")
+        print(f"TO ADD:{diff+value}")
+        if diff != '':
+            if "x" in self.operations[value]:
+                append = self.operations[value].replace("x", diff,1)
+            else:
+                append = [diff, value]
+
+            self.entry_b.insert(END, self.operations[value].replace("x", diff,1))
+            self.equation += append
